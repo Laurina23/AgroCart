8ce74c75d1f916db13bafc4d668da1706fec34c6
Laurina-p <laurinapatnaik20@gmail.com>
2025-07-22 11:52:56 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/payment" element={<Payment/>} />
         <Route path="/profile" element={<ProfilePage/>} />
-
       </Routes>
     </BrowserRouter>
   );
